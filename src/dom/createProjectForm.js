02c425319f2ce953137dd805cbda95d3026b7cee
Jack Watters <johnwatt@usc.edu>
2023-01-01 22:54:32 -0800

    cleaned up create project form and project section

--- src/dom/createProjectForm.js
+++ src/dom/createProjectForm.js
@@ -1,28 +1,10 @@
 export default () => {
-  const sections = document.querySelector('.sections');
+  const addProjectBtn = document.querySelector('.add-project-button');
+  const addProjectForm = document.querySelector('.add-project');
+  const projectNameInput = document.querySelector('.project-input');
 
-  // Create actual project column
-  const section = document.createElement('div');
-  section.classList.add('add-project');
-
-  // add form html
-  const header = document.createElement('form');
-  header.classList.add('project-form');
-  header.id = 'project-form';
-
-  const headerTitle = document.createElement('div');
-  headerTitle.classList.add('header-input');
-
-  const input = document.createElement('input');
-  input.placeholder = 'Name your project...';
-  input.required = true;
-  input.classList.add('project-input');
-  headerTitle.appendChild(input);
-  header.appendChild(headerTitle);
-
-  section.appendChild(header);
-  sections.appendChild(section);
-
-  // cursor clicks on the text
-  return header;
+  addProjectBtn.classList.add('hidden');
+  addProjectForm.classList.remove('hidden');
+  projectNameInput.focus();
+  projectNameInput.select();
 };
