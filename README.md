09caf30c5ee16a7aacefe8ad555477cb1c1a42ca
Jack Watters <johnwatt@usc.edu>
2023-01-01 23:11:11 -0800

    changes to index -> flow of app

--- README.md
+++ README.md
@@ -3,8 +3,6 @@
 This is a copy of what my todo board in notion looks like. It allows users to create projects and add todos to them. Todos and projects can be deleted and edited (actually project names cannot be edited rn I didn't even think of adding that).
 
 Notes
-- general click off of sidebar should still shut
-
 - loading local storage probably needs a shit ton of work
 - drag n drop
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,84 @@
+/* eslint-disable no-shadow */
 import './styles/style.css';
 import ProjectBoard from './js/classes/projectBoard';
-import loadContent from './js/functions/load-content';
-import { addForm } from './js/functions/add-project';
+// eslint-disable-next-line no-unused-vars
+import loadContent from './js/functions/loadLocalStorage';
+import Project from './js/classes/project';
+import saveToLocalStorage from './js/functions/saveLocalStorage';
+import createProject from './dom/createProject';
+import createProjectForm from './dom/createProjectForm';
+import hideProjectForm from './dom/hideProjectForm';
+import createTodoForm from './dom/createTodoForm';
+import Todo from './js/classes/todo';
+import toggleSidebar from './dom/toggleSidebar';
+import clickOffSidebar from './dom/clickOffSidebar';
+import cardDragStart from './dom/cardDragStart';
+import cardDrop from './dom/cardDrop';
 
-const projectBoard = new ProjectBoard();
+const addProjectBtn = document.querySelector('.add-project-button');
+const addProjectForm = document.querySelector('.add-project');
+const projectNameInput = document.querySelector('.project-input');
 
-if (localStorage.getItem('Project Board')) loadContent(projectBoard);
+const projectBoard = new ProjectBoard(); // TODO i dont like this
+// if (localStorage.getItem('Project')) loadContent(projectBoard);
 
-const addSection = document.querySelector('.add-project-button');
-addSection.addEventListener('click', () => {
-  addForm(projectBoard);
+document.addEventListener('click', clickOffSidebar);
+addProjectBtn.addEventListener('click', createProjectForm);
+
+addProjectForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const project = new Project(projectNameInput.value);
+  projectBoard.addProject(project);
+  const projectSection = createProject(project);
+
+  const deleteBtn = document.querySelector(`#${project.id}.delete`);
+  deleteBtn.addEventListener('click', () => {
+    projectSection.remove();
+    projectBoard.removeProjectFromBoard(project.id);
+    saveToLocalStorage(projectBoard);
+  });
+
+  const newTodoBtn = document.querySelector(`#${project.id}.section>.new`);
+  newTodoBtn.addEventListener('click', () => {
+    const todoForm = createTodoForm(project);
+    const todoInput = todoForm.firstElementChild;
+    const card = todoForm.parentElement;
+    const isDone = card.lastElementChild;
+
+    todoForm.addEventListener('submit', (e) => {
+      e.preventDefault();
+      if (!todoInput.value) card.remove();
+
+      const todo = new Todo(todoInput.value, project.id);
+      project.addTodo(todo);
+      saveToLocalStorage(projectBoard);
+      card.id = todoInput.value;
+      todoInput.blur();
+
+      todoInput.addEventListener('input', () => todo.setTitle(todoInput));
+      card.addEventListener('click', (e) => {
+        if (e.target.className !== 'done') toggleSidebar(todo);
+      });
+
+      isDone.addEventListener('click', () => {
+        todo.toggleDone();
+        card.remove();
+        saveToLocalStorage(projectBoard);
+      });
+    });
+    todoInput.addEventListener('blur', () => {
+      if (!todoInput.value) card.remove();
+    });
+
+    card.addEventListener('dragstart', cardDragStart);
+    card.addEventListener('dragover', (e) => e.preventDefault());
+    card.addEventListener('dragenter', (e) => e.preventDefault());
+    card.addEventListener('drop', (e) => {
+      const cardData = cardDrop(e);
+    });
+  });
+
+  saveToLocalStorage(projectBoard);
+  hideProjectForm();
 });
